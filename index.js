1e9791a725430ac8198c061f4fd8d7c1e431910b
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-02-22 13:32:04 +0200

    feature: created function for getting roots of square equations

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function getSquareEquationRoots(a, b, c) {
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) return null;
 
+  const sqrtOfDiscriminant = Math.sqrt(discriminant);
+  return {
+    x1: (-b + sqrtOfDiscriminant) / (2 * a),
+    x2: (-b - sqrtOfDiscriminant) / (2 * a),
+  };
+}
